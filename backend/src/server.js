ec17f9796b160761273606b61fb1f69454e1788c
Tenghuy <lytenghuy8@gmail.com>
2025-09-19 15:24:51 +0700

    change 4

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import itemRoutes from "./routes/itemRoutes.js";
 
 dotenv.config(); // MUST be at the very top
 
 const app = express();
 
 app.use(cors({
-  origin: "https://demo.lthtitan.online",
+  origin: "https://demo.lthtitan.online",  // frontend domain
   credentials: true,
-}));
+}))
 app.use(express.json());
 
 // Debug: check if environment variable is loaded
@@ -23,7 +24,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
-import itemRoutes from "./routes/itemRoutes.js";
 app.use("/api/items", itemRoutes);
 
 const PORT = process.env.PORT || 5000;
